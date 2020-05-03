ba6aa88635cf2fe60cd8f060abcba6d73d3e7bca
Norman Gilmore <git@teamforecast.com>
2020-05-03 15:19:38 -0700

    Configure queues for deploy to new account.

--- deploy-queues.sh
+++ deploy-queues.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws cloudformation package --template queues.yml --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-16by7zdqsml2x --output-template queues-export.yml
-aws cloudformation deploy --template-file queues-export.yml --stack-name TagQueueStack
+aws cloudformation package --template queues.yml --output-template queues-export.yml --s3-bucket cloudformation-sourcebucket-ztlebfosltkjbq
+aws cloudformation deploy --template-file queues-export.yml --stack-name pe-task-queues --parameter-overrides "KMSKey=public-editor-queues-key:1"
