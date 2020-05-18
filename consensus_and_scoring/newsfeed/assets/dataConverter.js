f415e17471d19675fe0dd294d34e2f222c11196a
Andy Flury <andrewflury@berkeley.edu>
2020-05-17 23:53:35 -0700

    Hallmark appears, same csv error

--- consensus_and_scoring/newsfeed/assets/dataConverter.js
+++ consensus_and_scoring/newsfeed/assets/dataConverter.js
@@ -4,21 +4,21 @@ function addDummyData(data) {
   var i = 0;
   //Get all categories that are non-empty.
   data.forEach((highlight) => {
-    if (highlight["Credibilty Indicator Category"]) {
-      categories.add(highlight["Credibilty Indicator Category"]);
+    if (highlight["Credibility Indicator Category"]) {
+      categories.add(highlight["Credibility Indicator Category"]);
       i ++;
     }
   });
   //Add all categories as nodes to the data with parent as CATEGORIES.
   categories.forEach((category) => {
-    data[i] = {"Credibilty Indicator Category": "CATEGORIES", "Credibility Indicator Name": category};
+    data[i] = {"Credibility Indicator Category": "CATEGORIES", "Credibility Indicator Name": category};
     i ++;
   })
       
   
 
   //Add root nodes.
-  data[i] = {"Credibilty Indicator Category": undefined, "Credibility Indicator Name": "CATEGORIES"};
+  data[i] = {"Credibility Indicator Category": undefined, "Credibility Indicator Name": "CATEGORIES"};
         
 
   return data;
@@ -30,7 +30,7 @@ function convertToHierarchy(data) {
 
   var stratify = d3.stratify()
     .id(d => d["Credibility Indicator Name"])
-    .parentId(d => d["Credibilty Indicator Category"])
+    .parentId(d => d["Credibility Indicator Category"])
     (data);
   //Hierarchy converts data to the same format that the D3 code expects.
   return d3.hierarchy(stratify);
