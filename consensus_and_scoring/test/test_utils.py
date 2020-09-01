e1ff3eb8fa2ab8603085ef9665c2ce27ab179964
Eric Wimsatt <ericwimsatt@gmail.com>
2020-09-01 15:21:29 -0700

    first commit

---
+++ consensus_and_scoring/test/test_utils.py
@@ -0,0 +1,27 @@
+def make_iaa_row():
+    return 0
+def make_row(columns):
+    return 0
+
+
+filetypes={
+    'iaa': {
+        'article_num': 0,
+        'article_sha256': 0,
+        'article_id': 0,
+        'article_filename': 0,
+        'source_task_uuid':0,
+        'tua_uuid': 0,
+        'namespace': 0,
+        'schema_sha256': 0,
+        'question_Number': 0,
+        'answer_uuid',
+        'question_type': 'default',
+        'agreed_Answer':0,
+        'coding_perc_agreement':0,
+        'one_two_diff':0,
+        'highlighted_indices':0,
+        'alpha_unitizing_score' L0	alpha_unitizing_score_inclusive	agreement_score	num_users	num_answer_choices	target_text	question_text	answer_text	answer_content
+
+    }
+}
