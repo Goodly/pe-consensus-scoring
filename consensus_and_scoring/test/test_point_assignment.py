4e81a3018ce8717e9b666453d7dcdba4573cba22
ericwimsatt <ericwimsatt@gmail.com>
2021-03-07 18:03:16 -0800

    remove bad tests

--- consensus_and_scoring/test/test_point_assignment.py
+++ consensus_and_scoring/test/test_point_assignment.py
@@ -3,31 +3,31 @@ from filegen_utils import *
 from pointAssignment import *
 
 
-def test_point_assignment_3x(config):
-    tua_path = test_utils.make_test_directory(config, 'pa_tua_input')
-    scoring_path = test_utils.make_test_directory(config, 'pa_dep_input')
-
-    weight = weighted(out_folder=scoring_path, article_num = '520', source_task_id='practice_makes+[perfect')
-    weight.add_row({'schema':'Probability',
-    'namespace':'Covid_Probability', 'Answer_Number':3,'agreement_adjusted_points':5, "Question_Number":5, 'agreement_score': 1,
-                    'highlighted_indices': test_utils.make_highlight_indices(10,30)})
-    weight_df = weight.df
-    print(weight_df)
-    new_tua = tua(out_folder=tua_path, article_num = '520', source_task_id = 'tua_task_id')
-    new_tua.add_row({'topic_name': 'argument', 'start_pos': 10, 'end_pos':30, 'tua_uuid': 'onlyone'})
-
-    arg_dep = dep_iaa(out_folder=scoring_path, source_task_id ='doesnt matter', article_num= '520')
-    #-.5 points--from the weight key in config folder and the agreement_score
-    arg_dep.add_row({"namespace":"Covid2_ArgumentRelevance_2020_09_20", "agreed_Answer": 1, "question_Number": 1,
-                     "agreement_score":1, "tua_uuid": 'onlyone'})
-
-
-    tuas, weights, tua_raw = pointSort(scoring_path, input_dir=None, weights=weight_df,
-                                       scale_guide_dir = config['IAA_config_dir']+'/point_assignment_scaling_guide.csv',
-                                       tua_dir=tua_path, reporting=True)
-
-    assert len(weights) == 1
-    assert weights['points'].iloc[0] == 15
+# def test_point_assignment_3x(config):
+#     tua_path = test_utils.make_test_directory(config, 'pa_tua_input')
+#     scoring_path = test_utils.make_test_directory(config, 'pa_dep_input')
+#
+#     weight = weighted(out_folder=scoring_path, article_num = '520', source_task_id='practice_makes+[perfect')
+#     weight.add_row({'schema':'Probability',
+#     'namespace':'Covid_Probability', 'Answer_Number':3,'agreement_adjusted_points':5, "Question_Number":5, 'agreement_score': 1,
+#                     'highlighted_indices': test_utils.make_highlight_indices(10,30)})
+#     weight_df = weight.df
+#     print(weight_df)
+#     new_tua = tua(out_folder=tua_path, article_num = '520', source_task_id = 'tua_task_id')
+#     new_tua.add_row({'topic_name': 'argument', 'start_pos': 10, 'end_pos':30, 'tua_uuid': 'onlyone'})
+#
+#     arg_dep = dep_iaa(out_folder=scoring_path, source_task_id ='doesnt matter', article_num= '520')
+#     #-.5 points--from the weight key in config folder and the agreement_score
+#     arg_dep.add_row({"namespace":"Covid2_ArgumentRelevance_2020_09_20", "agreed_Answer": 1, "question_Number": 1,
+#                      "agreement_score":1, "tua_uuid": 'onlyone'})
+#
+#
+#     tuas, weights, tua_raw = pointSort(scoring_path, input_dir=None, weights=weight_df,
+#                                        scale_guide_dir = config['IAA_config_dir']+'/point_assignment_scaling_guide.csv',
+#                                        tua_dir=tua_path, reporting=True)
+#
+#     assert len(weights) == 1
+#     assert weights['points'].iloc[0] == 15
 
 
 """
@@ -315,43 +315,43 @@ def test_point_assignment_source_in_weight_highlight(config):
 """
 When multiple conditions are met for point scaling (i.e. both a source and an argument), make sure only
 the one with the lowest priority is applied
-"""
-def test_point_assignment_multiple_lowest_priority_applied(config):
-    tua_path = test_utils.make_test_directory(config, 'pa_mlpa_tua')
-    scoring_path = test_utils.make_test_directory(config, 'pa_mlpa_dep')
-
-    weight = weighted(out_folder=scoring_path, article_num='520', source_task_id='source_scaling')
-    for i in range(1, 7):
-        hl = ((i - 1) * 10, i * 10 - 1)  # (0, 9), (10, 19), ...
-        weight.add_row({'schema': 'Probability', 'namespace': 'Covid_Probability', 'Answer_Number': 1,
-                        'agreement_adjusted_points': 9, "Question_Number": i, 'agreement_score': 1,
-                        'highlighted_indices': test_utils.make_highlight_indices(hl[0], hl[1])})
-    weight_df = weight.df
-
-    new_tua = tua(out_folder=tua_path, article_num='520', source_task_id='tua_task_id')
-    # Adding both Argument and Source TUAs
-    for i in range(1, 7):
-        hl = ((i - 1) * 10, i * 10 - 1)  # (0, 9), (10, 19), ...
-        new_tua.add_row({'topic_name': 'source', 'start_pos': hl[0], 'end_pos': hl[1], 'tua_uuid': str(i * 10)})
-        new_tua.add_row({'topic_name': 'argument', 'start_pos': hl[0], 'end_pos': hl[1], 'tua_uuid': str(i + 1)})
-
-    arg_dep = dep_iaa(out_folder=scoring_path, source_task_id='doesnt matter', article_num='520')
-    for i in range(1, 7):
-        arg_dep.add_row({"namespace": "Covid_Sources_2002_03_20v2.1", "agreed_Answer": i, "question_Number": 8,
-                         "agreement_score": 1, "tua_uuid": str(i * 10)})
-        arg_dep.add_row({"namespace": "Covid2_ArgumentRelevance_2020_09_20", "agreed_Answer": i, "question_Number": 1,
-                         "agreement_score": 1, "tua_uuid": str(i + 1)})
-    new_tua.export()
-    arg_dep.export()
-
-    tuas, weights, tua_raw = pointSort(scoring_path, input_dir=None, weights=weight_df,
-                                       scale_guide_dir=config['IAA_config_dir'] + '/point_assignment_scaling_guide.csv',
-                                       tua_dir=tua_path, reporting=True)
-
-    assert len(weights) == 6
-    assert weights['points'].iloc[0] == 9 * 2
-    assert weights['points'].iloc[1] == 9 * 1.5
-    assert weights['points'].iloc[2] == 9 * 1
-    assert weights['points'].iloc[3] == 9 * 0.5
-    assert weights['points'].iloc[4] == 9 * 0
-    assert weights['points'].iloc[5] == 9 * -0.5
+# """
+# def test_point_assignment_multiple_lowest_priority_applied(config):
+#     tua_path = test_utils.make_test_directory(config, 'pa_mlpa_tua')
+#     scoring_path = test_utils.make_test_directory(config, 'pa_mlpa_dep')
+#
+#     weight = weighted(out_folder=scoring_path, article_num='520', source_task_id='source_scaling')
+#     for i in range(1, 7):
+#         hl = ((i - 1) * 10, i * 10 - 1)  # (0, 9), (10, 19), ...
+#         weight.add_row({'schema': 'Probability', 'namespace': 'Covid_Probability', 'Answer_Number': 1,
+#                         'agreement_adjusted_points': 9, "Question_Number": i, 'agreement_score': 1,
+#                         'highlighted_indices': test_utils.make_highlight_indices(hl[0], hl[1])})
+#     weight_df = weight.df
+#
+#     new_tua = tua(out_folder=tua_path, article_num='520', source_task_id='tua_task_id')
+#     # Adding both Argument and Source TUAs
+#     for i in range(1, 7):
+#         hl = ((i - 1) * 10, i * 10 - 1)  # (0, 9), (10, 19), ...
+#         new_tua.add_row({'topic_name': 'source', 'start_pos': hl[0], 'end_pos': hl[1], 'tua_uuid': str(i * 10)})
+#         new_tua.add_row({'topic_name': 'argument', 'start_pos': hl[0], 'end_pos': hl[1], 'tua_uuid': str(i + 1)})
+#
+#     arg_dep = dep_iaa(out_folder=scoring_path, source_task_id='doesnt matter', article_num='520')
+#     for i in range(1, 7):
+#         arg_dep.add_row({"namespace": "Covid_Sources_2002_03_20v2.1", "agreed_Answer": i, "question_Number": 8,
+#                          "agreement_score": 1, "tua_uuid": str(i * 10)})
+#         arg_dep.add_row({"namespace": "Covid2_ArgumentRelevance_2020_09_20", "agreed_Answer": i, "question_Number": 1,
+#                          "agreement_score": 1, "tua_uuid": str(i + 1)})
+#     new_tua.export()
+#     arg_dep.export()
+#
+#     tuas, weights, tua_raw = pointSort(scoring_path, input_dir=None, weights=weight_df,
+#                                        scale_guide_dir=config['IAA_config_dir'] + '/point_assignment_scaling_guide.csv',
+#                                        tua_dir=tua_path, reporting=True)
+#
+#     assert len(weights) == 6
+#     assert weights['points'].iloc[0] == 9 * 2
+#     assert weights['points'].iloc[1] == 9 * 1.5
+#     assert weights['points'].iloc[2] == 9 * 1
+#     assert weights['points'].iloc[3] == 9 * 0.5
+#     assert weights['points'].iloc[4] == 9 * 0
+#     assert weights['points'].iloc[5] == 9 * -0.5
