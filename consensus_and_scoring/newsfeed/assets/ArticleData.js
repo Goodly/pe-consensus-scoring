1b456074d0cd5a5f815bea8058894ab80e318908
Andy Flury <andrewflury@berkeley.edu>
2021-05-22 13:24:10 -0700

    Missing title error fixed, dates reformated

--- consensus_and_scoring/newsfeed/assets/ArticleData.js
+++ consensus_and_scoring/newsfeed/assets/ArticleData.js
@@ -2,7 +2,7 @@ class ArticleData {
     constructor(title, author, date, ID, articleLink, visLink, plainText, highlightData) {
         this.title = title;
         this.author = author;
-        this.date = date.slice(0, -18);
+        this.date = date;
         this.id = ID;
         this.articleLink = articleLink;
         this.plainText = plainText;
